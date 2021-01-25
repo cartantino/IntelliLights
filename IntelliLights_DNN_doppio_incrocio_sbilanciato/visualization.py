6b442b9e710d0026559afc164e314f3c4b48432f
Costantino <ccarta@systemasrl.it>
2021-01-25 23:41:50 +0100

    DNN Doppio incrocio

---
+++ IntelliLights_DNN_doppio_incrocio_sbilanciato/visualization.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import os
+
+class Visualization:
+    def __init__(self, path_1, path_2, dpi):
+            self._path_1 = path_1
+            self._path_2 = path_2
+            self._dpi = dpi
+
+
+    def save_data_and_plot_1(self, data, filename, xlabel, ylabel):
+        """
+        Produce a plot of performance of the agent over the session and save the relative data to txt
+        """
+        min_val = min(data)
+        max_val = max(data)
+
+        plt.rcParams.update({'font.size': 24})  # set bigger font size
+
+        plt.plot(data)
+        plt.ylabel(ylabel)
+        plt.xlabel(xlabel)
+        plt.margins(0)
+        plt.ylim(min_val - 0.05 * abs(min_val), max_val + 0.05 * abs(max_val))
+        fig = plt.gcf()
+        fig.set_size_inches(20, 11.25)
+        fig.savefig(os.path.join(self._path_1, 'plot_'+filename+'.png'), dpi=self._dpi)
+        plt.close("all")
+
+        with open(os.path.join(self._path_1, 'plot_'+filename + '_data.txt'), "w") as file:
+            for value in data:
+                    file.write("%s\n" % value)
+
+    def save_data_and_plot_2(self, data, filename, xlabel, ylabel):
+        """
+        Produce a plot of performance of the agent over the session and save the relative data to txt
+        """
+        min_val = min(data)
+        max_val = max(data)
+
+        plt.rcParams.update({'font.size': 24})  # set bigger font size
+
+        plt.plot(data)
+        plt.ylabel(ylabel)
+        plt.xlabel(xlabel)
+        plt.margins(0)
+        plt.ylim(min_val - 0.05 * abs(min_val), max_val + 0.05 * abs(max_val))
+        fig = plt.gcf()
+        fig.set_size_inches(20, 11.25)
+        fig.savefig(os.path.join(self._path_2, 'plot_'+filename+'.png'), dpi=self._dpi)
+        plt.close("all")
+
+        with open(os.path.join(self._path_2, 'plot_'+filename + '_data.txt'), "w") as file:
+            for value in data:
+                    file.write("%s\n" % value)
